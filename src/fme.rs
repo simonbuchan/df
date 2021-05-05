6718a120621cec2197ad48f645cdaf16cfb499ec
Simon Buchan <simon.buchan@skilitics.com>
2021-05-06 00:58:52 +1200

    Decode FME format.

---
+++ src/fme.rs
@@ -0,0 +1,88 @@
+use std::io;
+
+use super::*;
+
+pub struct Fme {
+    pub offset: (i32, i32),
+    pub flip: bool,
+    pub unit_size: (u32, u32),
+    pub size: (u32, u32),
+    pub data: Vec<u8>,
+}
+
+impl Fme {
+    pub fn read(mut file: impl io::Read + io::Seek) -> io::Result<Self> {
+        let x_offset = read_i32(&mut file)?;
+        let y_offset = read_i32(&mut file)?;
+        let flip = read_u32(&mut file)? != 0;
+        let data_properties_offset = read_u32(&mut file)?;
+        let unit_width = read_u32(&mut file)?;
+        let unit_height = read_u32(&mut file)?;
+        // read_u32(&mut file)?; // padding
+        // read_u32(&mut file)?; // padding
+
+        file.seek(io::SeekFrom::Start(data_properties_offset as u64))?;
+
+        let width = read_u32(&mut file)?;
+        let height = read_u32(&mut file)?;
+        let compressed = read_u32(&mut file)? != 0;
+        let data_size = read_u32(&mut file)?;
+        let data_offset = read_i32(&mut file)?;
+        read_u32(&mut file)?; // padding
+
+        let pos = file.stream_position()?;
+
+        file.seek(io::SeekFrom::Current(data_offset as i64))?;
+
+        let mut columns = Vec::with_capacity(data_size as usize);
+        if !compressed {
+            file.read_to_end(&mut columns)?;
+        } else {
+            let mut column_offsets = Vec::new();
+
+            for _ in 0..width {
+                column_offsets.push(data_properties_offset + read_u32(&mut file)?);
+            }
+
+            // rle0::decompress(file, height, column_offsets)
+            let mut buffer = [0u8; 128];
+            for offset in column_offsets {
+                file.seek(io::SeekFrom::Start(offset as u64))?;
+                let mut unpacked_bytes = 0;
+                while unpacked_bytes < height {
+                    let mut control_byte = 0u8;
+                    file.read_exact(std::slice::from_mut(&mut control_byte))?;
+                    if control_byte <= 128 {
+                        columns.extend_from_slice(
+                            read_buf(&mut file, &mut buffer[0..control_byte as usize])?,
+                        );
+                    } else {
+                        control_byte -= 128;
+                        for _ in 0..control_byte {
+                            columns.push(0);
+                        }
+                    }
+                    unpacked_bytes += control_byte as u32;
+                }
+            }
+        }
+
+        assert_eq!(columns.len(), (width * height) as usize);
+
+        // data is in columns, bottom to top, not rows. Transpose it.
+        let mut data = Vec::with_capacity(columns.len());
+        for y in 0..height as usize {
+            for x in 0..width as usize {
+                data.push(columns[x * height as usize + height as usize - y - 1]);
+            }
+        }
+
+        Ok(Self {
+            offset: (x_offset, y_offset),
+            flip,
+            unit_size: (unit_width, unit_height),
+            size: (width, height),
+            data,
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::io::{self, prelude::*};
 use eframe::egui;
 
 use error::*;
+use std::path::Path;
 
 mod error;
 
@@ -21,12 +22,22 @@ struct Selected {
     index: usize,
     name: String,
     length: u32,
-    data: String,
+    text_data: String,
+    decoded: Decoded,
 }
+
+enum Decoded {
+    Unknown,
+    Fme { fme: fme::Fme, texture_id: egui::TextureId },
+}
+
+mod fme;
+
+const BASE_PATH: &str = r"C:\Games\Steam\steamapps\common\Dark Forces\Game\";
 
 impl App {
     fn new() -> ReadResult<Self> {
-        let mut file = File::open(r"C:\Games\Steam\steamapps\common\Dark Forces\Game\DARK.GOB")?;
+        let mut file = File::open(Path::new(BASE_PATH).join("SPRITES.GOB"))?;
         let catalog = gob::Catalog::read(&mut file)?;
         Ok(Self { file, catalog, selected: None })
     }
@@ -56,7 +67,7 @@ impl eframe::epi::App for App {
         "Dark Forces Explorer"
     }
 
-    fn update(&mut self, ctx: &egui::CtxRef, _frame: &mut eframe::epi::Frame) {
+    fn update(&mut self, ctx: &egui::CtxRef, frame: &mut eframe::epi::Frame) {
         egui::SidePanel::left(1, 200.0).show(ctx, |ui| {
             ui.heading("Entries");
             egui::ScrollArea::auto_sized().show(ui, |ui| {
@@ -70,12 +81,43 @@ impl eframe::epi::App for App {
                         entry.data(&mut self.file)
                             .read_to_end(&mut data)
                             .expect("can read file");
-                        let data = Self::escape_text_data(data);
+
+                        let decoded = if entry.name().ends_with(".FME") {
+                            match fme::Fme::read(io::Cursor::new(&data)) {
+                                Ok(fme) => {
+                                    let colors = fme.data.iter()
+                                        .map(|c| egui::Color32::from_gray(*c))
+                                        .collect::<Vec<_>>();
+                                    let texture_id = frame.tex_allocator()
+                                        .alloc_srgba_premultiplied(
+                                            (fme.size.0 as usize, fme.size.1 as usize),
+                                            &colors,
+                                        );
+                                    Decoded::Fme {
+                                        fme,
+                                        texture_id,
+                                    }
+                                }
+                                Err(..) => Decoded::Unknown,
+                            }
+                        } else {
+                            Decoded::Unknown
+                        };
+
+                        let text_data = Self::escape_text_data(data);
+
+                        if let Some(Selected { decoded, .. }) = &self.selected {
+                            if let Decoded::Fme { texture_id, .. } = decoded {
+                                frame.tex_allocator().free(*texture_id);
+                            }
+                        }
+
                         self.selected = Some(Selected {
                             index,
                             name: entry.name().to_string(),
                             length: entry.length(),
-                            data,
+                            text_data,
+                            decoded,
                         });
                     }
                 }
@@ -89,8 +131,28 @@ impl eframe::epi::App for App {
                 }
                 Some(selected) => {
                     ui.heading(format!("Selected: {:?} ({} bytes)", selected.name, selected.length));
-                    egui::ScrollArea::auto_sized().show(ui, |ui| {
-                        ui.label(&selected.data);
+                    ui.horizontal(|ui| {
+                        match &selected.decoded {
+                            Decoded::Unknown => {}
+                            Decoded::Fme { fme, texture_id } => {
+                                ui.vertical(|ui| {
+                                    ui.horizontal(|ui| {
+                                        ui.label(format!("offset {}x{}", fme.offset.0, fme.offset.1));
+                                        ui.label(format!("flip {}", fme.flip));
+                                        ui.label(format!("unit {}x{}", fme.unit_size.0, fme.unit_size.1));
+                                        ui.label(format!("size {}x{}", fme.size.0, fme.size.1));
+                                    });
+                                    ui.image(*texture_id, egui::Vec2::new(
+                                        fme.size.0 as f32 * 4.0,
+                                        fme.size.1 as f32 * 4.0,
+                                    ));
+                                });
+                            }
+                        }
+
+                        egui::ScrollArea::auto_sized().show(ui, |ui| {
+                            ui.label(&selected.text_data);
+                        });
                     });
                 }
             }
@@ -99,6 +161,10 @@ impl eframe::epi::App for App {
 }
 
 mod gob;
+
+fn read_i32(input: impl io::Read) -> io::Result<i32> {
+    Ok(i32::from_le_bytes(read_buf(input, [0u8; 4])?))
+}
 
 fn read_u32(input: impl io::Read) -> io::Result<u32> {
     Ok(u32::from_le_bytes(read_buf(input, [0u8; 4])?))
