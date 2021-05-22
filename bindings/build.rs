3c65c6e22ca87b534fa617f9b5ab398c3febacb3
Simon Buchan <simon.buchan@skilitics.com>
2021-05-22 22:08:17 +1200

    Move df-bindings to standard bindings path.

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,8 +1,6 @@
 fn main() {
     windows::build! {
         Windows::Foundation::*,
-        Windows::Storage::Streams::Buffer,
         Windows::Devices::Midi::*,
-        Windows::Win32::System::WinRT::IMemoryBufferByteAccess,
     }
 }
